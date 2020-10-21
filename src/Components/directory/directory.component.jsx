1336d16056e85b48f1c82721d96357925d4c6a5f
me701203 <me701203@gmail.com>
2020-10-21 16:39:00 +0330

    withRouter for HATS PAGE is done

--- src/Components/directory/directory.component.jsx
+++ src/Components/directory/directory.component.jsx
@@ -11,29 +11,34 @@ class Directory extends React.Component {
                 {
                     title: 'hats',
                     imageURL: 'https://i.ibb.co/cvpntL1/hats.png',
-                    id: 1
+                    id: 1,
+                    linkUrl: 'hats'
                 },
                 {
                     title: 'jackets',
                     imageURL: 'https://i.ibb.co/px2tCc3/jackets.png',
-                    id: 2
+                    id: 2,
+                    linkUrl: ''
                 },
                 {
                     title: 'sneakers',
                     imageURL: 'https://i.ibb.co/0jqHpnp/sneakers.png',
-                    id: 3
+                    id: 3,
+                    linkUrl: ''
                 },
                 {
                     title: 'womens',
                     imageURL: 'https://i.ibb.co/GCCdy8t/womens.png',
                     size: 'large',
-                    id: 4
+                    id: 4,
+                    linkUrl: ''
                 },
                 {
                     title: 'mens',
                     imageURL: 'https://i.ibb.co/R70vBrQ/men.png',
                     size: 'large',
-                    id: 5
+                    id: 5,
+                    linkUrl: ''
                 }
             ] 
         }
@@ -42,8 +47,8 @@ class Directory extends React.Component {
         return(
             <div className="directory-menu">
                 {
-                    this.state.sections.map(({title, imageURL, id, size}) => (
-                            <MenuItem key={id} title={title} imageUrl={imageURL} size= {size}/>
+                    this.state.sections.map(({title, imageURL, id, size, linkUrl}) => (
+                            <MenuItem key={id} title={title} imageUrl={imageURL} size= {size} linkUrl={linkUrl}/>
                         )
                     )
                 }
